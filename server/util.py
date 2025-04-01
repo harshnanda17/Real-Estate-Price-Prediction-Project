6d783ba4c1b0b40ca87f8ad8b2cc8b85795cb3e0
harshnanda17 <harshnanda954846179@gmail.com>
2025-04-01 14:15:37 +0530

    add files

--- server/util.py
+++ server/util.py
@@ -1,4 +1,5 @@
 import pickle
+import json
 import numpy as np
 
 __locations = None
@@ -10,29 +11,30 @@ def get_estimated_price(location,sqft,bhk,bath):
         loc_index = __data_columns.index(location.lower())
     except:
         loc_index = -1
+
     x = np.zeros(len(__data_columns))
     x[0] = sqft
-    x[1] = bhk
-    x[2] = bath
-    if loc_index >= 0:
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
         x[loc_index] = 1
 
-def get_location_names():
-    with open('server/locations.csv', 'r') as f:
-        locations = f.read().splitlines()
+    return round(__model.predict([x])[0],2)
 
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global __data_columns
+    global  __data_columns
     global __locations
 
-    with open("./artifacts/columns.json", 'r') as f:
-        __data_columns = json.load(f) ['data_columns']
-        __data_columns = json.load(f) ['data']
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
-    with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
-        __model = pickle.load(f)
+    global __model
+    if __model is None:
+        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+            __model = pickle.load(f)
     print("loading saved artifacts...done")
 
 def get_location_names():
@@ -44,4 +46,7 @@ def get_data_columns():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_data_columns())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
